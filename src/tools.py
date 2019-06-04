e1a7e6d18d639011788e89febea79eaef91a9234
Mariano Ferrero <marianoferrero.mf@gmail.com>
2019-06-04 14:31:37 -0300

    Add solutions and micro changes to content

--- src/tools.py
+++ src/tools.py
@@ -48,7 +48,7 @@ def plot_confusion_matrix(cm,
     plt.figure(figsize=(8, 6))
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
-    plt.colorbar()
+    #plt.colorbar()
 
     if target_names is not None:
         tick_marks = np.arange(len(target_names))
@@ -62,7 +62,7 @@ def plot_confusion_matrix(cm,
     thresh = cm.max() / 1.5 if normalize else cm.max() / 2
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         if normalize:
-            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+            plt.text(j, i, "{:0.2f}".format(cm[i, j]),
                      horizontalalignment="center",
                      color="white" if cm[i, j] > thresh else "black")
         else:
@@ -73,5 +73,5 @@ def plot_confusion_matrix(cm,
 
     plt.tight_layout()
     plt.ylabel('True label')
-    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.xlabel('Predicted label\naccuracy={:0.2f}; misclass={:0.2f}'.format(accuracy, misclass))
     plt.show()
